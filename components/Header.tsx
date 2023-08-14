3961092658e5b5321e34b2d604834e85d7754303
vhlongon <vhlongon@gmail.com>
2023-08-14 20:11:39 +0200

    refactor Header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,40 +1,17 @@
 import { UserButton, auth } from '@clerk/nextjs';
-import { headers } from 'next/headers';
-import { SignInButton } from './SignInButton';
-
-const isWhiteListedPage = () => {
-  const headersList = headers();
-  const pathname = headersList.get('x-invoke-path') || '';
-  const blackList = ['/sign-in', '/sign-up', '/'];
-  return !blackList.includes(pathname);
-};
+import { SignOutButton } from './SignOutButton';
 
 export const Header = () => {
   const { userId } = auth();
-  const showUserInfo = isWhiteListedPage();
 
   return (
     <header className="flex justify-between p-4">
-      {showUserInfo ? (
-        <>
-          <h1>Workout generator</h1>
-          {userId ? (
-            <div className="flex gap-4">
-              <UserButton
-                afterSignOutUrl="/"
-                userProfileUrl="/dashboard"
-                showName
-                signInUrl="/sign-in"
-                userProfileMode="navigation"
-              />
-              {/* <SignOutButton /> */}
-            </div>
-          ) : (
-            <SignInButton />
-          )}
-        </>
-      ) : (
-        <div>This is a closed beta</div>
+      <h1>Workout generator</h1>
+      {userId && (
+        <div className="flex gap-4">
+          <UserButton showName userProfileMode="navigation" />
+          <SignOutButton />
+        </div>
       )}
     </header>
   );
