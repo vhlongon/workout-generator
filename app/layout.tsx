fdae9ee6bcf9f9f1a74e1ca7a3db363620a2ab9e
vhlongon <vhlongon@gmail.com>
2023-08-14 20:17:36 +0200

    set app background

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import { Header } from '../components/Header';
 import './globals.css';
+import { twMerge } from 'tailwind-merge';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -18,13 +19,13 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="en" data-theme="synthwave">
       <ClerkProvider
         appearance={{
           baseTheme: shadesOfPurple,
         }}
       >
-        <body className={inter.className}>
+        <body className={twMerge(inter.className, 'bg-base-100')}>
           <div className="flex min-h-[100dvh] flex-col">
             <Header />
             <main className="flex w-full flex-grow">{children}</main>
