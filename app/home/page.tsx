3938320b367744b2db0048994d3d33fd525fbf04
vhlongon <vhlongon@gmail.com>
2023-08-17 00:37:33 +0200

    add TODOS

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -6,7 +6,7 @@ const Homepage = async () => {
   const user = await currentUser();
 
   return (
-    <div className="w-full flex justify-center items-center">
+    <div className="w-full flex flex-col justify-center items-center">
       {user ? (
         <h2 className="text-lg font-semibold">
           Welcome back{' '}
@@ -29,6 +29,20 @@ const Homepage = async () => {
           </div>
         </div>
       )}
+
+      <div className="flex flex-col gap-4 mt-16 border border-gray-500 rounded p-4 text-gray-500">
+        <span className="text-center block font-bold">Upcomming features:</span>
+        <ul>
+          <li>
+            {/* TODO: Add sorting and filter  */}
+            <code>- Sort and filter workouts</code>
+          </li>
+          <li>
+            {/* TODO: Add favourites  */}
+            <code>- Add favourite workouts</code>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 };
