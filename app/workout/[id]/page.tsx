8138302e80af7777a7d0321c0489f44fde7e4e40
vhlongon <vhlongon@gmail.com>
2023-08-17 21:29:48 +0200

    fix workout specific page

--- app/workout/[id]/page.tsx
+++ app/workout/[id]/page.tsx
@@ -1,6 +1,7 @@
 import { getWorkoutAction } from '@/actions/getWorkoutAction';
 import UpdateWorkoutFlow from './UpdateWorkoutFlow';
 import { formatWorkoutData } from '@/helpers/format';
+import { ErrorMessage } from '@/components/ErrorMessage';
 
 type WorkoutPageProps = {
   params: {
@@ -11,11 +12,19 @@ const WorkoutPage = async ({ params }: WorkoutPageProps) => {
   const { data, error } = await getWorkoutAction(params.id);
 
   if (error) {
-    return <div>something went wrong: {error}</div>;
+    return (
+      <div className="w-full max-w-fit mx-auto flex items-center justify-center">
+        <ErrorMessage>{error}</ErrorMessage>
+      </div>
+    );
   }
 
   if (!data) {
-    return <div>Workout not found</div>;
+    return (
+      <div className="w-full flex flex-col items-center justify-center">
+        <h2 className="text-center text-2xl">Workout not found</h2>
+      </div>
+    );
   }
 
   return (
