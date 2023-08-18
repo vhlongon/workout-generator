e56b588bb0ae92806074f571fe9a882f0d155ee0
vhlongon <vhlongon@gmail.com>
2023-08-18 17:07:26 +0200

    tweak favourite container

--- app/home/FavouriteWorks.tsx
+++ app/home/FavouriteWorks.tsx
@@ -17,7 +17,7 @@ export const FavouriteWorks = async () => {
       </div>
       {Boolean(favouriteWorkouts.length) ? (
         <div className="flex justify-center">
-          <ul className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 max-w-5xl w-full gap-2 mx-auto">
+          <ul className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 max-w-5xl w-full gap-2 mx-auto border border-base-200 rounded-2xl p-4">
             {favouriteWorkouts.map(workout => {
               const { name } = workout;
               return (
